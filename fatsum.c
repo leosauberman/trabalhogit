eaef734748fa396380c88555e1235262f404badf
Leonardo Sauberman <leosauberman@gmail.com>
2016-07-28 08:58:38 -0300

    creates main function and the logical part

--- fatsum.c
+++ fatsum.c
@@ -5,3 +5,15 @@ long long int fat(long long int n)
 	if(n == 0 || n == 1)return 1;
 	return n*fat(n-1);	
 }
+
+int main()
+{
+	int n;
+	int m;
+	int fatsum;
+	scanf("%d", n);
+	scanf("%d", m);
+	fatsum= fat(n) + fat(m);
+	printf("%d\n", fatsum);
+	return 0;
+}
